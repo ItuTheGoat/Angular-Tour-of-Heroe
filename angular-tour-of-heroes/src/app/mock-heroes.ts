02b55c8c9953807ee2c205f7edaaa6e9cffadfca
Developer <developer@Assemble-M1-Mac-mini.local>
2022-04-05 16:33:48 +0200

    Initial commit.

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Batman' },
+  { id: 12, name: 'Superman' },
+  { id: 13, name: 'Wonder Woman' },
+  { id: 14, name: 'Green Lantern' },
+  { id: 15, name: 'Flash' },
+];
